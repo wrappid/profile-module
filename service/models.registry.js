b6bcf4f6890e8559e339056a1866caf36b10942a
PritamIT2023 <pritammondal8981@email.com>
2024-01-08 11:01:00 +0530

    feat: Add Models
    AddressTypes model added
    
    Ref: #18

--- service/models.registry.js
+++ service/models.registry.js
@@ -1,4 +1,9 @@
+const AddressTypes = require("./models/AddressTypes.model");
 const modelsRegistry = {
+  AddressTypes: {
+    database: "application",
+    model   : AddressTypes
+  }
 };
 
 exports.modelsRegistry = modelsRegistry;
