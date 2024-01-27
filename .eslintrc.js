e1d3a8d3be1f8e713f5e4ca0f007c054d0c61c6e
PritamIT2023 <pritammondal8981@email.com>
2024-01-27 08:42:46 +0530

    refactor: update template
    Merge remote-tracking branch 'template/main' into WRPD-enhancement-22
    
    Ref: #22

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-  "env": {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 "env": {
     "browser": true,
     "es2021" : true,
     "node"   : true
@@ -37,7 +36,7 @@ module.exports = {
     "import/order"             : [
       "error",
       {
-        "alphabetize": {
+        "alphabetize" : {
           "caseInsensitive": true,
           "order"          : "asc",
         },
@@ -49,59 +48,11 @@ module.exports = {
             "pattern" : "react",
             "position": "before",
           },
-        ],
-        "pathGroupsExcludedImportTypes": ["react"],
-      },
-    ],
-    "indent"                  : ["error", 2, { "MemberExpression": 1, "SwitchCase": 1 }],
-    "key-spacing"             : ["error", { "align": "colon" }],
-    "linebreak-style"         : ["error", "unix"],
-    "newline-after-var"       : ["error", "always"],
-    "newline-per-chained-call": ["error", { "ignoreChainWithDepth": 3 }],
-    "no-console"              : "error",
-    "no-multi-spaces"         : ["error", { exceptions: { "VariableDeclarator": true } }],
-    "no-multiple-empty-lines" : ["error", { "max": 1 }],
-    "no-unused-vars"          : ["error", { "args": "after-used", "vars": "all" }],
-    "no-var"                  : "error",
-    "object-curly-newline"    : [
-      "error",
-      {
-        "ExportDeclaration": { "minProperties": 6, "multiline": true },
-        "ImportDeclaration": { "minProperties": 6, "multiline": true },
-        "ObjectExpression" : { "minProperties": 6, "multiline": true },
-        "ObjectPattern"    : { "minProperties": 6, "multiline": true },
-      },
-    ],
-    "object-curly-spacing"           : ["error", "always"],
-    "object-property-newline"        : ["error", { "allowAllPropertiesOnSameLine": true }],
-    "padding-line-between-statements": [
-      "error",
-      {
-        blankLine: "always",
-        next     : "*",
-        prev     : ["const", "let", "var"],
-      },
-      {
-        blankLine: "any",
-        next     : ["const", "let", "var"],
-        prev     : ["const", "let", "var"],
-      },
-      {
-        blankLine: "always",
-        next     : "*",
-        prev     : ["case", "default"],
-      }
-    ],
-    "quotes"                          : ["error", "double"],
-    "react/jsx-first-prop-new-line"   : "error",
-    "react/jsx-max-props-per-line"    : ["error", { "maximum": { "multi": 1, "single": 3 } }],
-    "react/jsx-newline"               : "error",
-    "react/jsx-props-no-multi-spaces" : "error",
-    "react/prop-types"                : "off",
-    "semi"                            : ["error", "always"],
-    "sort-keys-fix/sort-keys-fix"     : "error",
-    "space-infix-ops"                 : ["error", { "int32Hint": false }],
-    "unused-imports/no-unused-imports": "error",
-  },
-  "settings": { "react": { "version": "detect" } },
-};
+          {
+            files: ["app/**/*.js"],
+            // Extend or merge with the app-specific configuration
+            ...require("./.eslint/app/.eslintrc.js"),
+          },
+    ]
+  }],
+},}
