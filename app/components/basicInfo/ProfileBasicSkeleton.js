f723042aef9809d3dacc9d9ac77e43afc7d5ebec
anantakumarghosh <antaghosh@gmail.com>
2024-02-05 11:25:01 +0530

    refactor(config): :rotating_light: fix linting
    fix linting errors
    
    Ref: #25

--- app/components/basicInfo/ProfileBasicSkeleton.js
+++ app/components/basicInfo/ProfileBasicSkeleton.js
@@ -1,24 +1,24 @@
 import { CoreSkeleton, CoreBox, CoreGrid, CoreClasses } from "@wrappid/core";
 
 export default function ProfileBasicSkeleton() {
-    return (
-        <CoreGrid>
-            <CoreBox
-                styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}
-                gridProps={{ gridSize: 2 }}
-            >
-                <CoreSkeleton variant="circular" height={70} width={70} />
-            </CoreBox>
+  return (
+    <CoreGrid>
+      <CoreBox
+        styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}
+        gridProps={{ gridSize: 2 }}
+      >
+        <CoreSkeleton variant="circular" height={70} width={70} />
+      </CoreBox>
 
-            <CoreBox gridProps={{ gridSize: 5 }}>
-                <CoreSkeleton variant="text" />
+      <CoreBox gridProps={{ gridSize: 5 }}>
+        <CoreSkeleton variant="text" />
 
-                <CoreSkeleton variant="text" />
-            </CoreBox>
+        <CoreSkeleton variant="text" />
+      </CoreBox>
 
-            <CoreBox gridProps={{ gridSize: 12 }}>
-                <CoreSkeleton variant="rectangular" height={50} />
-            </CoreBox>
-        </CoreGrid>
-    );
+      <CoreBox gridProps={{ gridSize: 12 }}>
+        <CoreSkeleton variant="rectangular" height={50} />
+      </CoreBox>
+    </CoreGrid>
+  );
 }
