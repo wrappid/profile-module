c7a6661f8b0cf7fb2ea119a9da0602ffb9ee37b3
Swrnv-qc <royswarnava5@gmail.com>
2024-02-07 12:03:09 +0530

    fix: :lipstick: added proper colours and font size
    added proper colours and font size

--- app/components/basicInfo/BasicInfoCard.js
+++ app/components/basicInfo/BasicInfoCard.js
@@ -13,12 +13,98 @@ import {
 } from "@wrappid/core";
 import { useSelector } from "react-redux";
 
-import { getAge /**, getFullName  */ } from "../utils/helper";
-
 export default function BasicInfoCard(props) {
   const {
     firstName, middleName, lastName, gender, dob, bio, photo
   } = props;
+  const getAge = (birthDate)=> {
+    let dob = new Date(birthDate);
+  
+    //extract the year, month, and date from date input
+    let dobYear = dob.getYear();
+  
+    let dobMonth = dob.getMonth();
+  
+    let dobDate = dob.getDate();
+  
+    //get the current date from the system
+    let now = new Date();
+  
+    //extract the year, month, and date from current date
+    let currentYear = now.getYear();
+  
+    let currentMonth = now.getMonth();
+  
+    let currentDate = now.getDate();
+  
+    //declare a variable to collect the age in year, month, and days
+    let age = {};
+  
+    let dateAge = 0;
+  
+    let monthAge = 0;
+  
+    //get years
+    let yearAge = currentYear - dobYear;
+  
+    //get months
+    if (currentMonth >= dobMonth) {
+      //get months when current month is greater
+      monthAge = currentMonth - dobMonth;
+    } else {
+      yearAge--;
+      monthAge = 12 + currentMonth - dobMonth;
+    }
+  
+    //get days
+    if (currentDate >= dobDate) {
+      //get days when the current date is greater
+      dateAge = currentDate - dobDate;
+    } else {
+      monthAge--;
+      dateAge = 31 + currentDate - dobDate;
+  
+      if (monthAge < 0) {
+        monthAge = 11;
+        yearAge--;
+      }
+    }
+    //group the age in a single variable
+    age = {
+      days  : dateAge,
+      months: monthAge,
+      years : yearAge,
+    };
+  
+    let ageString = "";
+  
+    if (age.years > 0) {
+      ageString = age.years + " year";
+      if (age.years > 0) {
+        ageString += "s";
+      }
+    }
+    if (age.months > 0) {
+      if (ageString) ageString += ", ";
+      ageString += age.months + " month";
+      if (age.months > 1) {
+        ageString += "s";
+      }
+    }
+  
+    //show days only if year is 0 (zero)
+    if (age.years > 0 === false) {
+      if (age.days > 0) {
+        if (ageString) ageString += ", ";
+        ageString += age.days + " day";
+        if (age.days > 1) {
+          ageString += "s";
+        }
+      }
+    }
+    //return the calculated age
+    return ageString && ageString.length > 0 ? ageString : "N/A";
+  };
 
   const getGender = (gender, iconFlag = false) => {
     let genderTmp = "unknown";
@@ -55,19 +141,19 @@ export default function BasicInfoCard(props) {
   const degrees = useSelector((state) => state?.profile?.registration?.degrees);
 
   return (
-    <CoreGrid styleClasses={[]}>
+    <CoreGrid styleClasses={[CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER]}>
       <CoreBox
         gridProps={{ gridSize: { md: 2, xs: 12 } }}
-        styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER, CoreClasses.BORDER.BORDER_PRIMARY]}
+        styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}
       >
         <CoreAvatar
-          styleClasses={[CoreClasses.DATA_DISPLAY.AVATAR_LARGE]}
+          styleClasses={[CoreClasses.DATA_DISPLAY.AVATAR_XLARGE]}
           src={photo}
         />
       </CoreBox>
 
-      <CoreBox gridProps={{ gridSize: { md: 10, xs: 12 } }} styleClasses={[]}>
-        <CoreH6>{getFullName({ firstName, lastName, middleName })}</CoreH6>
+      <CoreBox gridProps={{ gridSize: { md: 10, xs: 12 } }}>
+        <CoreH6 styleClasses={[CoreClasses.COLOR.TEXT_BLACK_50]}>{getFullName({ firstName, lastName, middleName })}</CoreH6>
 
         {/* Display degrees */}
         <CoreTypographyBody1>{degrees}</CoreTypographyBody1>
@@ -75,14 +161,32 @@ export default function BasicInfoCard(props) {
         <CoreStack
           direction="row"
           spacing={1}
-          styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_FLEX_START]}
+          styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_FLEX_START, CoreClasses.ALIGNMENT.ALIGN_ITEMS_CENTER]}
         >
           
-          <CoreIconText type={__IconTypes.MATERIAL_OUTLINED_ICON} icon="cake" text={getAge(dob)} />
+          <CoreBox styleClasses={[CoreClasses.COLOR.TEXT_SECONDARY]}>
+            <CoreIconText
+              type={__IconTypes.MATERIAL_OUTLINED_ICON}
+              icon="cake"
+              text={
+                <CoreBox styleClasses={[CoreClasses.COLOR.TEXT_BLACK_50]}>
+                  {getAge(dob)}  
+                </CoreBox>
+              } />
+          </CoreBox>
 
           <CoreTypographyCaption styleClasses={[CoreClasses.COLOR.TEXT_SECONDARY]}>{" | "}</CoreTypographyCaption>
 
-          <CoreIconText type={__IconTypes.MATERIAL_OUTLINED_ICON} icon={getGender(gender, true)} text={getGender(gender)} />
+          <CoreBox styleClasses={[CoreClasses.COLOR.TEXT_SECONDARY]}>
+            <CoreIconText 
+              type={__IconTypes.MATERIAL_OUTLINED_ICON} 
+              icon={getGender(gender, true)} 
+              text={
+                <CoreBox styleClasses={[CoreClasses.COLOR.TEXT_BLACK_50]}>
+                  {getGender(gender)}  
+                </CoreBox>
+              } />
+          </CoreBox>
 
         </CoreStack>
       </CoreBox>
