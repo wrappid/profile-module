5add4f673e53632f68ba50ef04fa8fb3c42bd6a1
PritamIT2023 <pritammondal8981@email.com>
2024-02-19 12:35:31 +0530

    feat: :fire: remove  models
    remove person related models
    
    Ref: #30

--- service/models.registry.ts
+++ service/models.registry.ts
@@ -1,12 +1,8 @@
 import { AddressTypes } from "./models/AddressTypes.model";
 import { PersonAddresses } from "./models/PersonAddresses.model";
-import { PersonContacts } from "./models/PersonContacts.model";
 import { PersonDocs } from "./models/PersonDocs.model";
 import { PersonEducations } from "./models/PersonEducations.model";
 import { PersonExperiences } from "./models/PersonExperiences.model";
-import { PersonRelations } from "./models/PersonRelations.model";
-import { Persons } from "./models/Persons.model";
-import { Relations } from "./models/Relations.model";
 
 const ModelsRegistry = {
   AddressTypes: {
@@ -16,10 +12,6 @@ const ModelsRegistry = {
   PersonAddresses: {
     database: "application",
     model: PersonAddresses,
-  },
-  PersonContacts: {
-    database: "application",
-    model: PersonContacts,
   },
   PersonDocs: {
     database: "application",
@@ -32,18 +24,6 @@ const ModelsRegistry = {
   PersonExperiences: {
     database: "application",
     model: PersonExperiences,
-  },
-  PersonRelations: {
-    database: "application",
-    model: PersonRelations,
-  },
-  Persons: {
-    database: "application",
-    model: Persons,
-  },
-  Relations: {
-    database: "application",
-    model: Relations,
   },
 };
 
