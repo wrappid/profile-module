0cc36d7aa9c5189ee8ed2c8da3381fe58207bc7e
PritamIT2023 <pritammondal8981@email.com>
2024-06-26 13:36:46 +0530

    feat(global): :lipstick: education and experience section UI polishing
    education and experience section UI polishing
    
    Ref: #59

--- app/components/education/EducationCard.js
+++ app/components/education/EducationCard.js
@@ -10,18 +10,21 @@ export default function EducationCard(props) {
   return (
     <CoreGrid>
       <CoreCardHeader
+        styleClasses={[CoreClasses.PADDING.P0]}  
         title={ 
-          <CoreTypographyBody1 styleClasses={[CoreClasses.TEXT.TEXT_WEIGHT_BOLD]}>
+          <CoreTypographyBody1 styleClasses={[CoreClasses.TEXT.TEXT_WEIGHT_SEMIBOLD, CoreClasses.MARGIN.MB1]}>
             {degree}
           </CoreTypographyBody1>
         }
         subheader={
           <CoreStack>
-            <CoreTypographySubtitle2>{school}</CoreTypographySubtitle2>
+            <CoreTypographySubtitle2 styleClasses={[CoreClasses.MARGIN.MB0]} >
+              {school}</CoreTypographySubtitle2>
 
-            <CoreTypographySubtitle2>{board + ", " + location}</CoreTypographySubtitle2>
+            <CoreTypographySubtitle2 styleClasses={[CoreClasses.MARGIN.MB0]}>
+              {board + ", " + location}</CoreTypographySubtitle2>
 
-            <CoreTypographyBody2>
+            <CoreTypographyBody2 styleClasses={[CoreClasses.TEXT.TEXT_WEIGHT_MEDIUM, CoreClasses.MARGIN.MB3]} >
               {viewFormattedDate(startDate) + " - " + viewFormattedDate(endDate, "endDate")}
             </CoreTypographyBody2>
           </CoreStack>
--- app/components/experience/ExperienceCard.js
+++ app/components/experience/ExperienceCard.js
@@ -13,18 +13,21 @@ export default function ExperienceCard(props) {
 
   return (
     <CoreCardHeader
+      styleClasses={[CoreClasses.PADDING.P0]}  
       title={
         <>
-          <CoreTypographyBody1 styleClasses={[CoreClasses.TEXT.TEXT_WEIGHT_BOLD]}>{designation}</CoreTypographyBody1>
+          <CoreTypographyBody1 styleClasses={[CoreClasses.TEXT.TEXT_WEIGHT_BOLD, CoreClasses.MARGIN.MB1]}>{designation}</CoreTypographyBody1>
         </>
       }
       subheader={
         <CoreStack>
-          <CoreTypographySubtitle2>{organization}</CoreTypographySubtitle2>
+          <CoreTypographySubtitle2 styleClasses={[CoreClasses.MARGIN.MB0]} >
+            {organization}</CoreTypographySubtitle2>
 
-          <CoreTypographySubtitle2>{location}</CoreTypographySubtitle2>
+          <CoreTypographySubtitle2 styleClasses={[CoreClasses.MARGIN.MB0]} >
+            {location}</CoreTypographySubtitle2>
 
-          <CoreTypographyBody2>
+          <CoreTypographyBody2 styleClasses={[CoreClasses.TEXT.TEXT_WEIGHT_MEDIUM, CoreClasses.MARGIN.MB3]} >
 
             {viewFormattedDate(startDate) + " - " + viewFormattedDate(endDate, "endDate")}
           </CoreTypographyBody2>
