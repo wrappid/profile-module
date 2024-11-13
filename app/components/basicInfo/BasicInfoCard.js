0b1ecd9f009c1b6a49aa79712c4b5d8d1c72be69
Pritam Bag <130136389+PritamBag@users.noreply.github.com>
2024-11-13 17:07:41 +0530

    fix: :bug: some border style property name changed, update this to resolve style changes

--- app/components/basicInfo/BasicInfoCard.js
+++ app/components/basicInfo/BasicInfoCard.js
@@ -79,7 +79,7 @@ export default function BasicInfoCard(props) {
     <CoreGrid styleClasses={[CoreClasses.MARGIN.ML1, CoreClasses.MARGIN.MT_N5]}>
       <CoreBox
         gridProps={{ gridSize: { md: 0.9, xs: 12 } }}
-        styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER, CoreClasses.BORDER.BORDER_PRIMARY]}
+        styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER, CoreClasses.BORDER.BORDER_COLOR_PRIMARY]}
       >
         <CoreAvatar
           styleClasses={[CoreClasses.DATA_DISPLAY.AVATAR_LARGE]}
